c5e45fa076ac26ecbbb4be8799cc8c4cd6951c58
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-13 12:30:29 +0530

    Add project files.

---
+++ Data/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(5);
+            c1.CalculateArea();
+            Console.WriteLine(c1);
+
+
+            Rectangle r1 = new Rectangle(5,6);
+            r1.CalculateArea();
+            Console.WriteLine(r1);
+
+        }
+    }
+}
